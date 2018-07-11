710fc100ea2b73f45646b936ff2a18cc7576df5d
jdgregson <jdgregson@gmail.com>
2018-07-10 20:52:33 -0700

    Implemented many new things, including:
     - ssh string editing in a dialog
     - ssh string editing in notepad
     - copying connections
     - renaming connections
     - better help organization
     - getter command processing
     - auto-creating connection folder

--- sshmgr.ps1
+++ sshmgr.ps1
@@ -4,6 +4,7 @@
 
 $CONNECTION_FOLDER = "~\Documents\sshmgr"
 $global:saved_connections = @{}
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 
 
 function Get-SavedConnections() {
@@ -11,7 +12,14 @@ function Get-SavedConnections() {
 }
 
 
+function Get-Input($message, $connectionName, $defaultValue) {
+    return [Microsoft.VisualBasic.Interaction]::InputBox(
+        $message, $connectionName, $defaultValue)
+}
+
+
 function Show-SavedConnections() {
+    $global:saved_connections = Get-SavedConnections
     Write-Host "Saved Connections:"
     if($global:saved_connections.Count -gt 0) {
         for($i = 0; $i -lt $global:saved_connections.Count; $i++) {
@@ -33,8 +41,8 @@ function Create-NewSavedConnection() {
         return
     }
     New-Item $file
-    "ssh -p 22 $name" > $file
-    notepad.exe $file
+    $(Get-Input "Enter the SSH string for $name" $name "ssh -p 22 $name") > $file
+    $global:saved_connections = Get-SavedConnections
 }
 
 
@@ -45,48 +53,103 @@ function Connect-SavedConnection($number) {
 
 function Delete-SavedConnection($number) {
     Remove-Item -Force $global:saved_connections[$number]
-    return Get-SavedConnections
+    $global:saved_connections = Get-SavedConnections
+}
+
+
+function Copy-SavedConnection($number) {
+    $name = Read-Host "Enter new saved connection name"
+    $file = "$CONNECTION_FOLDER\$name.txt"
+    Copy-Item $global:saved_connections[$number] $file
+    $global:saved_connections = Get-SavedConnections
+}
+
+
+function Edit-SavedConnection($number) {
+    $original_string = Get-Content $global:saved_connections[$number]
+    $name = $global:saved_connections[$number].Name
+    $new_string = Get-Input "Enter the SSH string for $name" $name $original_string
+    if($new_string) {
+        $new_string > $global:saved_connections[$number]
+    }
+    $global:saved_connections = Get-SavedConnections
+}
+
+
+function Edit-SavedConnectionNotepad($number) {
+    notepad.exe $global:saved_connections[$number] | Out-Null
+    $global:saved_connections = Get-SavedConnections
+}
+
+
+function Rename-SavedConnection($number) {
+    $original_name = $global:saved_connections[$number].Name
+    $new_name = Read-Host "Enter new name for $original_name"
+    if($new_name -and $new_name -ne $original_name) {
+        Rename-Item $global:saved_connections[$number] $new_name
+    }
+    $global:saved_connections = Get-SavedConnections
 }
 
 
 function Show-Help() {
     $help = New-Object System.Collections.ArrayList;
-    $help.Add($(New-Object PSObject -Prop @{'Command'='connect';'Description'='Connect to a saved connection';'Example'='connect 1';'Alias'='c'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='edit';'Description'='Edit a saved connection';'Example'='edit 1';'Alias'='e'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='new';'Description'='Create a new saved connection';'Example'='new';'Alias'='n'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='delete';'Description'='Delete a saved connection';'Example'='delete 1';'Alias'='d'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='copy';'Description'='Copy a saved connection';'Example'='copy 1';'Alias'='C'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='list';'Description'='List saved connections';'Example'='list';'Alias'='l'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='exit';'Description'='Exit sshmgr';'Example'='exit';'Alias'='exit, bye, x'})) | Out-Null
     $help.Add($(New-Object PSObject -Prop @{'Command'='args';'Description'='Show SSH arguments';'Example'='args';'Alias'=''})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='connect';'Description'='Connect to a saved connection';'Example'='connect 1';'Alias'='c'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='copy';'Description'='Copy a saved connection';'Example'='copy 1';'Alias'='C, cp'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='delete';'Description'='Delete a saved connection';'Example'='delete 1';'Alias'='d, rm, del'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='edit';'Description'='Edit a saved connection';'Example'='edit 1';'Alias'='e'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='exit';'Description'='Exit sshmgr';'Example'='exit';'Alias'='exit, bye, x'})) | Out-Null
     $help.Add($(New-Object PSObject -Prop @{'Command'='help';'Description'='Show this help message';'Example'='help';'Alias'='?, h'})) | Out-Null
-    $help.Add($(New-Object PSObject -Prop @{'Command'='x';'Description'='x';'Example'='x';'Alias'='x'})) | Out-Null
-    $help | Sort-Object Command | Format-Table
+    $help.Add($(New-Object PSObject -Prop @{'Command'='list';'Description'='List saved connections';'Example'='list';'Alias'='l, ls, dir'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='new';'Description'='Create a new saved connection';'Example'='new';'Alias'='n'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='notepad';'Description'='Edit a saved connection in notepad';'Example'='notepad 1';'Alias'='np'})) | Out-Null
+    $help.Add($(New-Object PSObject -Prop @{'Command'='rename';'Description'='Rename a saved connection';'Example'='rename 1';'Alias'='r, ren'})) | Out-Null
+    #$help.Add($(New-Object PSObject -Prop @{'Command'='x';'Description'='x';'Example'='x';'Alias'='x'})) | Out-Null
+    $help | Sort-Object Command | Format-Table -Property Command,Description,Example,Alias -AutoSize
 }
 
 
 function Process-Command($command) {
-    if($command -eq "args") {
+    $command = $command -split " ",2
+    $c = $command[0]
+    $arg = $command[1]
+    if($c -in "args") {
         ssh
-    } elseif($command -match "c \d") {
-        Connect-SavedConnection $($command -Replace "c ")
-    } elseif($command -eq "n") {
-        Create-NewSavedConnection
-        $global:saved_connections = Get-SavedConnections
-    } elseif($command -match "d \d") {
-        Delete-SavedConnection $($command -Replace "d ")
-    } elseif($command -eq "?" -or $command -eq "help") {
+    } elseif($c -in "connect","c") {
+        Connect-SavedConnection $arg
+    } elseif($c -in "copy","c","cp") {
+        Copy-SavedConnection $arg
+    } elseif($c -in "delete","rm","del","d") {
+        Delete-SavedConnection $arg
+    } elseif($c -in "edit","e") {
+        Edit-SavedConnection $arg
+    } elseif($c -in "exit","x","bye","quit") {
+        Exit
+    } elseif($c -in "?","help","h") {
         Show-Help
-    } elseif($command -eq "l" -or $command -eq "list") {
+    } elseif($c -in "list","l","ls","dir") {
         Show-SavedConnections
+    } elseif($c -in "new","n") {
+        Create-NewSavedConnection
+    } elseif($c -in "notepad","np") {
+        Edit-SavedConnectionNotepad $arg
+    }  elseif($c -in "rename","r","ren") {
+        Rename-SavedConnection $arg
+    } else {
+        Write-Host "Unknown command"
     }
+    Write-Host
 }
 
 
+if(-not(Test-Path $CONNECTION_FOLDER)) {
+    $out = mkdir $CONNECTION_FOLDER | Out-String
+}
 $global:saved_connections = Get-SavedConnections
 Show-SavedConnections
 Write-Host
-while($command = Read-Host "Enter command (enter ? for help)") {
-    Process-Command $command
-    Write-Host
+Write-Host "Enter ? for help"
+while($True) {
+    Process-Command $(Read-Host "[sshmgr] Enter command")
 }
